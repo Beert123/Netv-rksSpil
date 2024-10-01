b0b89780dca2d7fcabe4a186664e4a46b93ec0d2
Beert123 <bertil@selmas.dk>
2024-10-01 11:52:25 +0200

    Hej hej

--- game/ClientThread.java
+++ game/ClientThread.java
@@ -1,4 +1,21 @@
 package game;
 
-public class ClientThread extends Thread{
-}
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+    public class ClientThread extends Thread{
+        BufferedReader bufferedReader;
+        public ClientThread(BufferedReader bufferedReader) {
+            this.bufferedReader = bufferedReader;
+        }
+        public void run(){
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(System.in));
+
+            try {
+                System.out.println(inFromServer.readLine());
+            }
+            catch (Exception e){
+                e.printStackTrace();
+
+            }
+        }
+    }
